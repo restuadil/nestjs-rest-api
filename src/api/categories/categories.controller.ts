7bc86aa4164544371eccd525ae4319e3b8dd030a
restuadil <restuadil09@gmail.com>
2025-08-17 02:01:29 +0700

    feat(categories) : add getall category

--- src/api/categories/categories.controller.ts
+++ src/api/categories/categories.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Post, Query, UsePipes } from "@nestjs/common";
 
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { Logger } from "winston";
 
 import { Roles } from "src/common/decortators/roles.decorator";
+import { ZodPipe } from "src/common/pipe/zod.pipe";
 import { Role } from "src/types/role.type";
 import { ControllerResponse } from "src/types/web.type";
 
 import { CategoriesService } from "./categories.service";
 import { CreateCategoryDto } from "./dto/create-category.dto";
+import { QueryCategoryDto, QueryCategorySchema } from "./dto/query-category.dto";
 import { Category } from "./entities/category.entity";
 
 @Controller("categories")
@@ -28,5 +30,17 @@ export class CategoriesController {
     const result = await this.categoriesService.create(createCategoryDto);
 
     return { message: "Category created successfully", data: result };
+  }
+
+  @Get()
+  @UsePipes(new ZodPipe(QueryCategorySchema))
+  async findAll(
+    @Query() queryCategoryDto: QueryCategoryDto,
+  ): Promise<ControllerResponse<Category[]>> {
+    this.logger.info(`Categories Controller - findAll`);
+
+    const { data, meta } = await this.categoriesService.findAll(queryCategoryDto);
+
+    return { message: "Categories fetched successfully", data: data, meta: meta };
   }
 }
--- src/api/categories/categories.service.ts
+++ src/api/categories/categories.service.ts
@@ -1,14 +1,18 @@
 import { ConflictException, Inject, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 
-import { Model } from "mongoose";
+import { FilterQuery, Model } from "mongoose";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { Logger } from "winston";
 
+import { generateMeta } from "src/common/helpers/generate-meta";
 import { generateSlug } from "src/common/helpers/generate-slug";
 import { RedisService } from "src/common/redis/redis.service";
+import { ConfigService } from "src/config/config.service";
+import { Meta, PaginationResponse } from "src/types/web.type";
 
 import { CreateCategoryDto } from "./dto/create-category.dto";
+import { QueryCategoryDto } from "./dto/query-category.dto";
 import { Category } from "./entities/category.entity";
 
 @Injectable()
@@ -17,6 +21,7 @@ export class CategoriesService {
     @InjectModel(Category.name) private readonly categoryModel: Model<Category>,
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
     private readonly redisService: RedisService,
+    private readonly configService: ConfigService,
   ) {}
   private async getCategoryByNameOrSlugAndThrow(
     name: string,
@@ -42,5 +47,48 @@ export class CategoriesService {
       name,
       slug,
     });
+  }
+
+  async findAll(queryCategoryDto: QueryCategoryDto): Promise<PaginationResponse<Category>> {
+    this.logger.info(`Get all categories...`);
+
+    const categoriesCache = await this.redisService.get<PaginationResponse<Category>>(
+      `category:${JSON.stringify(queryCategoryDto)}`,
+    );
+    if (categoriesCache) return categoriesCache;
+
+    const { limit, order, page, search, sort } = queryCategoryDto;
+    const queryObject: FilterQuery<Category> = {};
+
+    if (search && search.length > 0) {
+      queryObject.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { slug: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    const [data, total] = await Promise.all([
+      this.categoryModel
+        .find(queryObject)
+        .sort({ [sort]: order })
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .lean()
+        .exec(),
+      this.categoryModel.countDocuments(queryObject).exec(),
+    ]);
+
+    const meta: Meta = generateMeta(page, limit, Number(total));
+
+    await this.redisService.set(
+      `category:${JSON.stringify(queryCategoryDto)}`,
+      { data, meta },
+      this.configService.get<number>("REDIS_TTL"),
+    );
+
+    return {
+      data,
+      meta,
+    };
   }
 }
