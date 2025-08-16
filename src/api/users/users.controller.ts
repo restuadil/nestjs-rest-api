c169a0022b05cfda6d5aa4544822e373a9fd4df8
cellby <cellby020202@gmail.com>
2025-08-16 09:07:14 +0700

    feat(users) : implement get user by id

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,8 +1,25 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Inject, Param } from "@nestjs/common";
 
+import { Types } from "mongoose";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import { Logger } from "winston";
+
+import { ControllerResponse } from "src/types/web.type";
+
+import { User } from "./entities/user.entitiy";
 import { UsersService } from "./users.service";
 
 @Controller("users")
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    private readonly usersService: UsersService,
+  ) {}
+  @Get(":id")
+  async findOne(@Param("id") id: Types.ObjectId): Promise<ControllerResponse<User>> {
+    this.logger.info(`Users Controller - findOne`);
+    const result = await this.usersService.findOne(new Types.ObjectId(id));
+
+    return { message: "User fetched successfully", data: result };
+  }
 }
--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+
+import { Model, Types } from "mongoose";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import { Logger } from "winston";
+
+import { RedisService } from "src/common/redis/redis.service";
+
+import { User } from "./entities/user.entitiy";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    private readonly redisService: RedisService,
+  ) {}
+  async findOne(id: Types.ObjectId): Promise<User> {
+    this.logger.info(`Get User...`);
+    const user = await this.userModel.findById(id);
+    if (!user) throw new NotFoundException("User not found");
+
+    return user;
+  }
+}
