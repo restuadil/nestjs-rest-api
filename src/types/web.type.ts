471e775d9f28a2b7692d5e8beaa6cdc9d09f16a4
restuadil <restuadil09@gmail.com>
2025-08-16 07:27:00 +0700

    fix(auth): set Jwt Env to number

---
+++ src/types/web.type.ts
@@ -0,0 +1,36 @@
+import { JwtPayload } from "./jwt.type";
+
+export interface ControllerResponse<T> {
+  message: string;
+  data: T | null;
+  meta?: Meta | null;
+}
+
+export interface BaseResponse<T> extends ControllerResponse<T> {
+  statusCode: number;
+  status: boolean;
+  error: string | null;
+  timestamp: string;
+  path: string;
+}
+
+export interface PaginationResponse<T> {
+  data: T[];
+  meta: Meta;
+}
+
+export interface Meta {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+  hasNext: boolean;
+  hasPrev: boolean;
+  nextPage?: number;
+  prevPage?: number;
+}
+
+export interface AuthRequest {
+  cookies: Record<string, string>;
+  user: JwtPayload;
+}
