87828aecae096f2d2f8f6e4238bb16eeabfed91e
restuadil <restuadil09@gmail.com>
2025-08-17 15:28:10 +0700

    fix(categories) : generate idParamSchema helper

--- src/common/helpers/idparam.dto.ts
+++ src/common/helpers/idparam.dto.ts
@@ -1,6 +1,8 @@
 import { Types } from "mongoose";
 import z from "zod";
 
-export const idParamSchema = z.string().refine((val) => Types.ObjectId.isValid(val), {
-  message: "Invalid ObjectId",
+export const idParamSchema = z.object({
+  id: z.string().refine((val) => Types.ObjectId.isValid(val), {
+    message: "Invalid ObjectId",
+  }),
 });
